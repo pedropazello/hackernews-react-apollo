0ec1b380222ba1858b4d1f6b9e4d86d8723c06ba
Pedro Pazello <email@pedropazello.com>
2018-05-01 11:12:32 -0300

    Add react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import logo from '../logo.svg';
 import '../styles/App.css';
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
+import Header from './Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <CreateLink />
-        <LinkList />
+      <div className="center w85">
+        <Header />
+        <div className="ph3 pv1 background-gray">
+          <Switch>
+            <Route exact path="/" component={LinkList} />
+            <Route exact path="/create" component={CreateLink} />
+          </Switch>
+        </div>
       </div>
     );
   }
