ebbd1edb27918c110a9fffdd5704d39f9c008d3b
Pedro Pazello <email@pedropazello.com>
2018-05-01 10:56:08 -0300

    Add CreateLink component with post mutation

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
-import LinkList from '../components/LinkList';
+import LinkList from './LinkList';
+import CreateLink from './CreateLink';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <CreateLink />
         <LinkList />
       </div>
     );
